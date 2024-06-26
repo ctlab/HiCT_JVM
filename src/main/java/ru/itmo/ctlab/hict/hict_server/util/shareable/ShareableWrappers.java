db35b2c3ff8aa42c7733c07c9d0cf9c897a3ca51
Aleksandr Serdiukov <ntwwwnt@gmail.com>
2024-06-27 02:50:14 +0400

    Updated headers

--- src/main/java/ru/itmo/ctlab/hict/hict_server/util/shareable/ShareableWrappers.java
+++ src/main/java/ru/itmo/ctlab/hict/hict_server/util/shareable/ShareableWrappers.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2024. Aleksandr Serdiukov, Anton Zamyatin, Aleksandr Sinitsyn, Vitalii Dravgelis and Computer Technologies Laboratory ITMO University team.
+ * Copyright (c) 2021-2024. Aleksandr Serdiukov, Anton Zamyatin, Aleksandr Sinitsyn, Vitalii Dravgelis and Computer Technologies Laboratory ITMO University team.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
